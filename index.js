72671cdb0b7ccb13844b93497ea7ee48894bfd6b
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 14:14:37 +0600

    get the all products

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-require('dotenv').config();
-const app=express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port =process.env.PORT || 5001;
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5001;
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xzyjuex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -16,22 +16,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    const database = client.db('ProductDB').collection('Products');
-    
-    app.post('/products',async(req,res)=>{
-        const product =req.body;
-        const result = await database.insertOne(product);
-       
-        res.send(result)
-    })
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const database = client.db("ProductDB").collection("Products");
+    app.get("/products", async (req, res) => {
+      const cursor = database.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await database.insertOne(product);
+
+      res.send(result);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,12 +45,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // routes
-app.get('/',(req,res)=>{
-    res.send('Api is working...');
-})
+app.get("/", (req, res) => {
+  res.send("Api is working...");
+});
 
-app.listen(port ,()=>{
-    console.log(`server is running on the http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`server is running on the http://localhost:${port}`);
+});
