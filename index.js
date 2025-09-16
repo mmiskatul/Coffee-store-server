e954b76ae484a60828fa24641d2137ea49a213ae
Md Miskatul Masabi <masabimisakat@gamil.com>
2025-09-16 10:04:46 +0600

    add product post

--- index.js
+++ index.js
@@ -21,14 +21,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+    const database = client.db('ProductDB').collection('Products');
+    
+    app.post('/products',async(req,res)=>{
+        const product =req.body;
+        const result = await database.insertOne(product);
+       
+        res.send(result)
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
